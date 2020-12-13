226be2c5499d5ee43c36bc1c33fc80e27ecc1c3d
Aritro Saha <29025984+Axiumin@users.noreply.github.com>
2020-12-13 15:17:00 -0500

    made files, will be testing designs in html file

---
+++ src/pages/NewLandingPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function NewLanding(props) {
+    return (
+        <div>
+            <header>
+                
+            </header>
+        </div>
+    );
+}
